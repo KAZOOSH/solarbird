4f5cd65526c67a8418c039a3639e126817496837
25mmHg <23mmhg@gmail.com>
2021-04-26 20:25:35 +0200

    new version with LFSR Noise

--- test/T85AllTest/T85AllTest/main.c
+++ test/T85AllTest/T85AllTest/main.c
@@ -38,7 +38,7 @@
  */ 
 
 #define F_CPU 8000000UL
-#define MAXDARKNESS 10000UL
+#define MAXDARKNESS 20000UL
 
 #define PIEPA PB3
 #define PIEPB PB4
@@ -48,6 +48,9 @@
 #define MYDDR DDRB
 #define MYPORT PORTB
 #define MYPIN PINB
+
+//#define SET(x) |=  (x)
+//#define CLR(x) &= ~(x)
 
 #include <avr/io.h>
 #include <avr/sleep.h>
@@ -66,84 +69,150 @@ void var_delay_10us(uint16_t ten_us)
 //Pieps
 void piepsen(uint16_t n, uint16_t t)
 {
-	MYPIN |= 1<<PIEPA;
+	MYDDR  |=  ((1<<PIEPA) | (1<<PIEPB));
+	MYPORT |=  (1<<PIEPA);
 	MYPORT &= ~(1<<PIEPB);
 	while(n)
 	{
 		n--;
-		MYPIN = (1<<PIEPB | 1<<PIEPA);
+		MYPIN |= ((1<<PIEPB) | (1<<PIEPA));
 		var_delay_10us(t);
 	}
+	MYPORT &= ~((1<<PIEPA) | (1<<PIEPB));
+	_delay_us(10);
+	// PINSTATE: booth piezopins are GND
 }
 
- //Lichtmessen
+ //How dark is it
 uint16_t get_darkness()
 {
-	uint16_t darkness = 0;	
+	uint16_t darkness = 0;
 	//booth junctions invers charging
-	MYPORT &= ~(1<<LEDANODE1 | 1<<LEDANODE2);
-	MYPORT |= 1<<LEDKATHODES;
+	MYDDR  |=  ((1<<LEDANODE1) | (1<<LEDANODE2) | (1<<LEDKATHODES));
+	MYPORT &= ~((1<<LEDANODE1) | (1<<LEDANODE2));
+	MYPORT |=  (1<<LEDKATHODES);
 	_delay_us(10);
 	//discharge junctions to HIGH
-	MYDDR &= ~(1<<LEDANODE1 | 1<<LEDANODE2);
-	while(!(MYPIN & (1<<LEDANODE1 | 1<<LEDANODE2)) && (darkness < MAXDARKNESS))
+	MYDDR &= ~((1<<LEDANODE1 | 1<<LEDANODE2));
+	while(!(MYPIN & ((1<<LEDANODE1) | (1<<LEDANODE2))) && (darkness < MAXDARKNESS))
 	{
 		darkness++;
 		_delay_us(10);
 	}
 	MYPORT &= ~(1<<LEDKATHODES);
-	MYDDR |= (1<<LEDANODE1 | 1<<LEDANODE2);
+	MYPORT &= ~((1<<LEDANODE1) | (1<<LEDANODE2));
+	MYDDR  |=  ((1<<LEDANODE1) | (1<<LEDANODE2) | (1<<LEDKATHODES));
 	return(darkness);
+	// PINSTATE: anodes are GND, kathodes are GND
 }
 
- //Flash
+ //Flash the LEDs
  void flash_leds(uint16_t ontime)
  {
-	 MYPORT |= (1<<LEDANODE1 | 1<<LEDANODE2);
+	 MYDDR  |= ((1<<LEDANODE1) | (1<<LEDANODE2) | (1<<LEDKATHODES));
+	 MYPORT &= ~(1<<LEDKATHODES);
+	 MYPORT |= ((1<<LEDANODE1) | (1<<LEDANODE2));
 	 var_delay_10us(ontime);
-	 MYPORT &= ~(1<<LEDANODE1 | 1<<LEDANODE2);
+	 MYPORT &= ~((1<<LEDANODE1) | (1<<LEDANODE2));
+	 // PINSTATE: anodes are GND, kathodes are GND
  }
 
 
-//WDT-Interrupt-init
-// START 8s TIMER, STROMVERBRAUCH 6 MIKROAMPRE
+// WDT-Interrupt-init
+// START 8s TIMER, current is 6uA
 void init_wdt()
 {
 	cli();
 	MCUSR &= ~(1<<WDRF);
-	WDTCR |= (1<<WDIE) | (1<<WDP3) | (1<<WDP0) ;
+	WDTCR |= ((1<<WDIE) | (1<<WDP3) | (1<<WDP0)) ;
 	wdt_reset();
 	sei();
 }
 
 
-//WDT-Interrupt
-ISR(WDT_vect)  // wakeup from sleeping
+// WDT-Interrupt, w<keup from sleeping without RESET
+ISR(WDT_vect)
 {
 	wdtcnt++;
 }
 
 int main(void)
 {
-    //init
-	MYDDR |= 1<<PIEPA;
-	MYDDR |= 1<<PIEPB;
-	MYDDR |= 1<<LEDANODE1;
-	MYDDR |= 1<<LEDANODE2;
-	MYDDR |= 1<<LEDKATHODES;
+    // init
+	MYDDR |= ((1<<PIEPA) | (1<<PIEPB));
+	MYDDR |= (1<<LEDKATHODES);
+	
+	// LOWLIGHT LEDs VIA PULLUP
+	MYPORT |= (1<<LEDANODE1);
+	MYPORT |= (1<<LEDANODE2);
+	_delay_ms(1000);
+	MYPORT &= ~(1<<LEDANODE1);
+	MYPORT &= ~(1<<LEDANODE2);
+	// PINSTATE: anodes are HiZ, kathodes and piezopins are GND
 	
 	set_sleep_mode(SLEEP_MODE_PWR_DOWN);
 	init_wdt();
 		
-	//Job
-	sei();
+	// Job	
 	while(1) 
     {
-		if(wdtcnt & 1)
+		if (wdtcnt == 1)
+		{
+			for (uint16_t i=0; i<1000; i++)
+			{
+				piepsen(10, (5+(get_darkness()>>3)));
+			}			
+		}
+		// MAGIC!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+		else if (wdtcnt & 2U)
+		{
+			// make 1s noise with LFSR
+			MYDDR  |=  ((1<<PIEPA) | (1<<PIEPB));
+			MYPORT |=  (1<<PIEPA);
+			MYPORT &= ~(1<<PIEPB);
+			uint16_t lfsr_state = 1;
+			for (uint16_t i=0; i<44100; i++)
+			{
+				_delay_us(23);
+				lfsr_state=(lfsr_state >> 1)^(-(lfsr_state & 1) & 0xB400u); // nach E.Galois
+				if (lfsr_state & 1)	MYPIN |= ((1<<PIEPB) | (1<<PIEPA));
+			}
+			MYPORT &= ~((1<<PIEPA) | (1<<PIEPB));
+			// PINSTATE: booth piezopins are GND
+			
+			piepsen(1000, 5);
+			
+			// make 10s noise filtered with 2 LFSR (flanger)
+			MYDDR  |=  ((1<<PIEPA) | (1<<PIEPB));
+			MYPORT |=  (1<<PIEPA);
+			MYPORT |=  (1<<PIEPB);
+			uint16_t lfsr_stateA = 44257; // Startwert für A
+			uint16_t lfsr_stateB = 38051; // Wert 100 Schritte vor A (B hat 2,3ms Vorsprung vor A)
+			for (uint16_t i=0; i<200; i++)
+			{
+				for (uint16_t j=0; j<2205; j++)
+				{
+					_delay_us(23);
+					// nach E.Galois
+					lfsr_stateA=(lfsr_stateA >> 1)^(-(lfsr_stateA & 1) & 0xB400U);
+					// jedes 200te Sample verpennen (Abstand A B verkürzt sich zu 0 und wird wieder länger)
+					if (j != 1) lfsr_stateB=(lfsr_stateB >> 1)^(-(lfsr_stateB & 1) & 0xB400U);
+					// Mixer
+					if (lfsr_stateA & 1) MYPIN |= (1<<PIEPA);
+					if (lfsr_stateB & 1) MYPIN |= (1<<PIEPB);
+				}
+			}
+			MYPORT &= ~((1<<PIEPA) | (1<<PIEPB));
+			// PINSTATE: booth piezopins are GND
+		// MAGIC END!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!	
+			
+		}
+		
+		else if(wdtcnt & 4U)
 		{
 			piepsen(1000, 5);
 			piepsen(25, 200);
-			piepsen(50, 500);
+			piepsen(50, 100);
 			piepsen(100, 50);
 			piepsen(200, 25);
 			piepsen(500, 10);
@@ -153,12 +222,26 @@ int main(void)
 			piepsen(1000, 5);
 		}
 		
-		else
+		else if (wdtcnt & 8U)
 		{
-			piepsen(1000, 5);
-			flash_leds(get_darkness());
-			piepsen(500, 10);
+			piepsen(600, 40);
+			piepsen(800, 30);
+			piepsen(600, 40);
+			_delay_ms(240);
+			piepsen(600, 40);
+			piepsen(800, 30);
+			
 		}
+		
+		else if (wdtcnt & 16U)
+		{
+			piepsen(300, 40);
+			flash_leds(get_darkness());
+			piepsen(400, 30);
+		}
+		else;
+		sei();
 		sleep_mode();
+		cli();
 	}
 }
