94a768aa35b2f78121de809577e884109cf4d5e5
Nick <nick@bitfasching.de>
2021-05-17 16:00:00 +0200

    LEDs: Add draft of brightness sensing (however unsatisfying, takes too long in darkness)

--- SolarbirdTiny/system/leds.h
+++ SolarbirdTiny/system/leds.h
@@ -47,4 +47,45 @@ class LEDs
 			bitWrite( DDRB, PIN_LED_2_ANODE, 0 );
 			bitWrite( DDRB, PIN_LED_CATHODES, 0 );
 		}
+
+		unsigned long static senseBrightness( const unsigned long dischargeTimeWindowMillis=300UL )
+		{
+			// make pins outputs
+			bitWrite( DDRB, PIN_LED_1_ANODE, 1 );
+			bitWrite( DDRB, PIN_LED_2_ANODE, 1 );
+			bitWrite( DDRB, PIN_LED_CATHODES, 1 );
+
+			// drive cathodes high and anodes low (inverse charging)
+			bitWrite( PORTB, PIN_LED_CATHODES, 1 );
+			bitWrite( PORTB, PIN_LED_1_ANODE, 0 );
+			bitWrite( PORTB, PIN_LED_2_ANODE, 0 );
+
+			// wait to charge
+			delay( 1 );
+
+			// change anodes to inputs (start discharging towards potential of cathodes)
+			bitWrite( DDRB, PIN_LED_1_ANODE, 0 );
+			bitWrite( DDRB, PIN_LED_2_ANODE, 0 );
+
+			// count time until potential of anodes is high
+			unsigned long dischargeStartMillis = millis();
+			unsigned long dischargeMillis = 0;
+			bool dischargedInTime = false;
+			while ( dischargeMillis <= dischargeTimeWindowMillis ) {
+				dischargeMillis = millis()-dischargeStartMillis;
+				if ( bitRead( PINB, PIN_LED_1_ANODE ) || bitRead( PINB, PIN_LED_2_ANODE ) ) {
+					dischargedInTime = true;
+					break;
+				}
+			}
+			if ( !dischargedInTime ) {
+				dischargeMillis = dischargeTimeWindowMillis;
+			}
+
+			// restore LED state
+			off();
+
+			// discharge time is inverse proportional to brightness
+			return dischargeTimeWindowMillis - dischargeMillis;
+		}
 };
